89de672bf8fd662801ac24381e5bda814062d596
robalexg <alexg7070@yahoo.com>
2022-10-25 23:51:47 -0500

    pixel-perfect

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,11 +4,13 @@ const Card = () => {
     return (
         <section className="card--container">
             <img className='card--image' src="../images/qrcode.png" alt="" />
-            <h1>We are an imaginative game studio specializing in simple Hyper-Casual mobile games!</h1>
+            <h1>
+                Improve your front-end skills by building projects
+            </h1>
     
             <h3>
-                If you're interested in creating something beautiful!
-                Scan the QR code and say Hello!!! 
+                Scan the QR code to visit Frontend Mentor and take your 
+                coding skills to the next level
             </h3>
         </section>
     )
